c6f4da78474ab18ce7ff08fa426efd6e9742f045
Dave Zimmelman <david.zimmelman@kronos.com>
2016-12-06 18:08:42 -0500

    Removed src/util

--- src/util/timestamp/timestamp.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (type='s') {
-    let t = new Date().getTime();
-
-    return type === 'ms' && t ||
-        type === 's' && Math.floor(t / 1000) ||
-        type === 'm' && Math.floor(t / 60000) ||
-        null;
-};
