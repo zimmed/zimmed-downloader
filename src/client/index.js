4c6eadf902449c5776b1e9e40d256facffc9f67b
Dave Zimmelman <david.zimmelman@kronos.com>
2016-12-09 10:54:00 -0500

    Moved server to client

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+const _ = require('lodash');
+const path = require('path');
+const SocketManager = require('zimmed-socket-client');
+const EventManager = require('zimmed-event');
+const Config = require('../config').governor;
+
+module.exports = (model) => {
+    let sm = SocketManager.create(
+        Config.host,
+        Config.port,
+        EventManager.create(Config.eventPath),
+        model,
+        Config.channel
+    );
+
+    return () => sm.init();
+};
